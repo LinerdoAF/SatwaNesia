126363e0f4db6c4c5f7d2e3dce4e861abd4c0663
LinerdoAF <agasthalinerdo@gmail.com>
2025-05-19 10:15:36 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.html", "./*.js"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1A1A1A",
+        teks: "#FAFAFA",
+        ijo: "#8BC34A",
+        abu: "#A1A1A1",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
